487954e0798bd7b422d9bcc1b04892d93921248f
Popa Nicoleta-Felicia <popanicoletafelicia19@gmail.com>
2025-03-11 11:04:14 +0200

    added some changes

---
+++ Seminar2/src/ro/cts/seminar2/clase/Angajat.java
@@ -0,0 +1,49 @@
+package ro.cts.seminar2.clase;
+
+import java.util.Arrays;
+
+public class Angajat extends Aplicant{
+    private String ocupatie;
+    private int salariu;
+    private static float sumaFinantata = 10;
+
+    public Angajat() {
+        super();
+    }
+
+    public Angajat(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiecte, int salariu, String ocupatie) {
+        super(nume, prenume, varsta, punctaj, nr_proiecte, denumireProiecte);
+        this.salariu = salariu;
+        this.ocupatie = ocupatie;
+    }
+
+    public String getOcupatie() {
+        return ocupatie;
+    }
+
+    public void setOcupatie(String ocupatie) {
+        this.ocupatie = ocupatie;
+    }
+
+    public int getSalariu() {
+        return salariu;
+    }
+
+    public void setSalariu(int salariu) {
+        this.salariu = salariu;
+    }
+
+    @Override
+    public String toString() {
+        return "Angajat: Nume=" + nume + ", Prenume=" + prenume
+                + ", Varsta=" + varsta + ", Punctaj=" + punctaj + ", Nr_proiecte=" + nr_proiecte + ", DenumireProiect="
+                + Arrays.toString(denumireProiect) + "Ocupatie=" + ocupatie + ", salariu=" + salariu;
+    }
+
+    @Override
+    public void afiseazaFinantarePrimita() {
+        System.out.println("Angajatul " + getNume() + " " + getPrenume() + " primeste" + sumaFinantata + " Euro/zi in proiect.");
+    }
+
+
+}
