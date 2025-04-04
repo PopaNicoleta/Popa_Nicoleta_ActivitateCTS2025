e1afdc827bd2c5f5a24586703897ad872ec51523
Popa Nicoleta-Felicia <popanicoletafelicia19@gmail.com>
2025-04-04 15:52:48 +0300

    singleton - thread safe

---
+++ AgeTur - lucru individual/src/SingletonThreadSafe/clase/Agentie.java
@@ -0,0 +1,28 @@
+package SingletonThreadSafe.clase;
+
+public class Agentie {
+    private String nume;
+    private int nrAngajati;
+    private String adresa;
+
+    private static Agentie instance = null;
+
+    private Agentie(String nume, int nrAngajati, String adresa) {
+        this.nume = nume;
+        this.nrAngajati = nrAngajati;
+        this.adresa = adresa;
+    }
+
+    public static synchronized Agentie getInstance(String nume, int nrAngajati, String adresa){
+        if(instance == null){
+            instance = new Agentie(nume, nrAngajati, adresa);
+        }
+        return instance;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Agentia ").append(nume).append(" are un numar de ").append(nrAngajati).append(" angajati si are sediul la adresa ").append(adresa).append(".");
+        return sb.toString();
+    }
+}
